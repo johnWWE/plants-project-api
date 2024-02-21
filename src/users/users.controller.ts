01bd4d28a97ddbdef548d160f51ae515190c0746
johnWWE <jhoncitof1ea10@gmail.com>
2024-02-20 23:29:41 -0500

    feat: add module, service and controller for Users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers(@Query('name') name?: string): Promise<User[]> {
+    return this.usersService.getAllUsers(name);
+  }
+
+  @Get(':id') getUserById(@Param('id') id: string): Promise<User> {
+    return this.usersService.getUserById(Number(id));
+  }
+
+  @Post() createUser(@Body() data: User): Promise<User> {
+    return this.usersService.createUser(data);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllUsers(name?: string): Promise<User[]> {
+    if (!name) return this.prisma.user.findMany();
+
+    return this.prisma.user.findMany({ where: { name: name } });
+  }
+
+  async getUserById(id: number): Promise<User> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async createUser(user: User): Promise<User> {
+    return this.prisma.user.create({ data: user });
+  }
+}
