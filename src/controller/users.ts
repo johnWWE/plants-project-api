8a9549e5ab72226a137402e7574b16fa5d3e6487
qwik-zgheib <qwikzgheib@gmail.com>
2024-03-29 16:09:04 -0500

    add fn_get_user_by_id

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -22,3 +22,28 @@ export const fn_get_all_users: GetAllUsersFunction = async (req, res) => {
   }
 };
 
+type GetUserByIdFunction = (req: Request, res: Response) => Promise<void>;
+
+export const fn_get_user_by_id: GetUserByIdFunction = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(400).json({ mensaje: 'User ID not provided' });
+      return;
+    }
+
+    const users: UserDocument | null = await User.findById(id);
+
+    if (!users) {
+      res.status(404).json({ mensaje: 'User not found' });
+      return;
+    }
+
+    res.status(200).json({ users });
+  } catch (error: any) {
+    console.error('Error getting user:', error);
+    res.status(500).json({ mensaje: 'Internal server error' });
+  }
+};
+
