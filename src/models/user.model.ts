a5f36b20f08dfcba9c2c75b8d70bc5c13739bbd0
johnWWE <jhoncitof1ea10@gmail.com>
2024-03-30 11:39:29 -0500

    add user models

---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { UserRole } from '../ts/interfaces';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: Object.values(UserRole), default: UserRole.NONE },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
