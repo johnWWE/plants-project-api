7d43c03093ad82b51f9112ea4940abb15c41c9a4
johnWWE <jhoncitof1ea10@gmail.com>
2024-03-30 11:41:07 -0500

    implement user routes in routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response, Router } from 'express';
+import express, { Router } from 'express';
+import userRouter from './user.routes';
 
 const router: Router = express.Router();
 
-router.get(`/`, (req: Request, res: Response) => {
-  res.send(`Welcome to plants project api`);
-});
+router.use('/users', userRouter);
 
 export default router;
