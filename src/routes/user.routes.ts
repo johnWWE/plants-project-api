10e3baf47f940aebb2435a092cea894985bf746c
qwik-zgheib <qwikzgheib@gmail.com>
2024-03-31 17:36:56 -0500

    feat: add update, patch, delete routes user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import express, { Router } from 'express';
-import { createUser, getUserById, getUsers } from '../controllers/user.controller';
+import { createUser, getUserById, getUsers, updateUser, roleUpdateUser, deleteUser } from '../controllers/user.controller';
 
 const router: Router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', createUser);
+router.put('/:userId', updateUser);
+router.patch('/:userId', roleUpdateUser);
+router.delete('/:userId', deleteUser);
 
 export default router;
