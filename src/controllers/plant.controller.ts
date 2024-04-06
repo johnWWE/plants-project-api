980fce6c526071ad29f622c2b3194aa3cc5612db
johnWWE <jhoncitof1ea10@gmail.com>
2024-04-06 14:26:17 -0500

    feat: plant routes succefully(more control is needed)

--- src/controllers/plant.controller.ts
+++ src/controllers/plant.controller.ts
@@ -7,6 +7,32 @@ import PlantLabel from '../models/plantLabel.model';
 import { BadRequestError, NotFoundError } from '../utils/customErrors';
 import { IPlant, IPlantLabel, RegexQuery } from '../ts/interfaces';
 
+export const getPlants: RequestHandler = async (req, res, next) => {
+  try {
+    const query: RegexQuery = {};
+    const { name } = req.query;
+    if (name) query.name = { $regex: name.toString(), $options: 'i' };
+
+    const plants: IPlant[] = await Plant.find(query).populate('label');
+
+    if (!plants.length) throw NotFoundError('plant(s) not found');
+
+    const dataPlants = plants.map((plant: IPlant) => {
+      const labels: string[] = plant.label.map((label: IPlantLabel) => label.label);
+      console.log(labels);
+
+      return {
+        ...plant._doc,
+        label: labels,
+      };
+    });
+
+    res.status(200).json(dataPlants as IPlant[]);
+  } catch (error) {
+    next(error as Error);
+  }
+};
+
 export const getPlant: RequestHandler = async (req, res, next) => {
   try {
     const id: string | undefined = req.params.id;
@@ -19,52 +45,7 @@ export const getPlant: RequestHandler = async (req, res, next) => {
 
     if (!plant) throw NotFoundError('Plant not found');
 
-    // const labels: string[] = plant.label.map((label: IPlantLabel) => label.label);
-
-    // const newPlant = {
-    //   _id: plant._id,
-    //   name: plant.name,
-    //   image: plant.image,
-    //   phallemia: plant.phallemia,
-    //   species: plant.species,
-    //   scientific_name: plant.scientific_name,
-    //   type: plant.type,
-    //   label: labels,
-    // };
-
-    // const newPlant = {
-    //   ...plant._doc,
-    //   label: labels,
-    // };
-
     res.status(200).json(plant);
-  } catch (error) {
-    next(error as Error);
-  }
-};
-
-export const getPlants: RequestHandler = async (req, res, next) => {
-  try {
-    const query: RegexQuery = {};
-    const { name } = req.query;
-    if (name) query.name = { $regex: name.toString(), $options: 'i' };
-
-    const plants: IPlant[] = await Plant.find(query).populate('label');
-
-    if (!plants.length) throw NotFoundError('plant(s) not found');
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const dataPlants: IPlant[] | any = plants.map((plant: IPlant) => {
-      const labels: string[] = plant.label.map((label: IPlantLabel) => label.label);
-      console.log(labels);
-
-      return {
-        ...plant._doc,
-        label: labels,
-      };
-    });
-
-    res.status(200).json(dataPlants);
   } catch (error) {
     next(error as Error);
   }
@@ -102,6 +83,64 @@ export const createPlant: RequestHandler = async (req, res, next) => {
     const savedPlant = await newPlant.save();
 
     res.status(201).json(savedPlant);
+  } catch (error) {
+    next(error as Error);
+  }
+};
+
+export const updatePlant: RequestHandler = async (req, res, next) => {
+  try {
+    const id: string | undefined = req.params.id;
+    const dataUpdate = req.body;
+    if (!id) throw BadRequestError('Plant id is required');
+
+    if (!Types.ObjectId.isValid(id)) throw NotFoundError('Plant id is invalid');
+
+    const currentPlant: IPlant | null = await Plant.findById(id);
+    if (!currentPlant) throw NotFoundError('Plant not found');
+
+    if (dataUpdate.label) {
+      let idLabels: Types.ObjectId[] = [];
+      if (Array.isArray(dataUpdate.label)) {
+        idLabels = await Promise.all(
+          dataUpdate.label.map(async (x: string) => {
+            const plantLabel: IPlantLabel | null = await PlantLabel.findOne({ label: x });
+            if (!plantLabel) throw NotFoundError(`Could not find label ${x}`);
+            return plantLabel.id;
+          }),
+        );
+      }
+      if (typeof dataUpdate.label === 'string') {
+        const plantLabel: IPlantLabel | null = await PlantLabel.findOne({ label: dataUpdate.label });
+        if (!plantLabel) throw NotFoundError('Label not found');
+        idLabels.push(plantLabel.id);
+      }
+
+      dataUpdate.label = idLabels;
+    }
+
+    const plantUpdate: IPlant = Object.assign(currentPlant, dataUpdate);
+
+    const savedPlant: IPlant = await plantUpdate.save();
+
+    res.status(200).send({ message: 'Update successfully', data: savedPlant });
+  } catch (error) {
+    next(error as Error);
+  }
+};
+
+export const deletePlant: RequestHandler = async (req, res, next) => {
+  try {
+    const id: string | undefined = req.params.id;
+    if (!id) throw BadRequestError('Plant id is required');
+    if (!Types.ObjectId.isValid(id)) throw NotFoundError('Plant id is invalid');
+
+    const currentPlant: IPlant | null = await Plant.findById(id);
+    if (!currentPlant) throw NotFoundError('Plant not found');
+
+    await Plant.findByIdAndDelete(id);
+
+    res.status(200).send({ message: 'plant deleted successfully' });
   } catch (error) {
     next(error as Error);
   }
--- src/routes/plant.routes.ts
+++ src/routes/plant.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { createPlant, getPlant, getPlants } from '../controllers/plant.controller';
+import { createPlant, deletePlant, getPlant, getPlants, updatePlant } from '../controllers/plant.controller';
 
 const router: Router = Router();
 
 router.get('/', getPlants);
 router.get('/:id', getPlant);
 router.post('/', createPlant);
+router.put('/:id', updatePlant);
+router.delete('/:id', deletePlant);
 
 export default router;
